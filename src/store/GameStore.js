47f0f1db9f8fc612d0e216adbae7e16677e1fc1b
Lukiano99 <l.stojadinovic99@gmail.com>
2024-02-02 00:45:11 +0100

    Models in Store updated

--- src/store/GameStore.js
+++ src/store/GameStore.js
@@ -2,11 +2,73 @@ import { configureStore, createSlice } from "@reduxjs/toolkit";
 
 const initialState = {
   levels: [
-    { status: "pending", id: 1, icon: "fa-solid fa-magnifying-glass" }, // ili 'success' ili 'failure'
-    { status: "pending", id: 2, icon: "fa-solid fa-fingerprint" },
-    { status: "pending", id: 3, icon: "fa-solid fa-question" },
-    { status: "pending", id: 4, icon: "fa-solid fa-memory" },
-    { status: "pending", id: 5, icon: "fa-solid fa-box-open" },
+    {
+      id: 1,
+      name: "Search Words",
+      status: "pending",
+      icon: "fa-solid fa-magnifying-glass",
+    }, 
+    {
+      id: 2,
+      name: "Cypher",
+      status: "pending",
+      icon: "fa-solid fa-fingerprint",
+    },
+    { status: "pending", id: 3, name: "Quiz", icon: "fa-solid fa-question" },
+    {
+      id: 4,
+      name: "Memory Game",
+      status: "pending",
+      icon: "fa-solid fa-memory",
+    },
+    {
+      id: 5,
+      name: "Puzzle Question",
+      status: "pending",
+      icon: "fa-solid fa-box-open",
+    },
+  ],
+  quizQuestions: [
+    {
+      question: "Šta predstavlja skraćenica HTML?",
+      options: [
+        "HyperText Markup Linguistics",
+        "HyperText Markup Language",
+        "HighTech Modular Language",
+        "HyperText Makeup Language",
+      ],
+      correctAnswer: 1,
+    },
+    {
+      question: "Koji je osnovni jezik za programiranje veb-stranica?",
+      options: ["JavaScript", "Python", "Ruby", "HTML5"],
+      correctAnswer: 0,
+    },
+    {
+      question: "Šta označava .CSS u web programiranju?",
+      options: [
+        "Computer Style Sheets",
+        "Creative Style Syntax",
+        "Colorful Styling System",
+        "Cascading Style Sheets",
+      ],
+      correctAnswer: 3,
+    },
+    {
+      question: "Koja je osnovna svrha HTTP u kontekstu internet komunikacije?",
+      options: [
+        "HyperText Transfer Protocol",
+        "High-Level Transfer Process",
+        "Human-Computer Text Protocol",
+        "HyperTextual Transmission Process",
+      ],
+      correctAnswer: 0,
+    },
+    {
+      question: "Koja je popularna baza podataka otvorenog koda?",
+      options: ["Oracle Database", "SQLite", "MySQL", "MongoDB"],
+      correctAnswer: 2,
+    },
   ],
 };
 const gameSlice = createSlice({
