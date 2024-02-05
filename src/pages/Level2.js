b8710557b31d681621a9232d40c9ecab014a76c8
Lukiano99 <l.stojadinovic99@gmail.com>
2024-02-06 00:19:54 +0100

    Level 2 - Cypher setup

--- src/pages/Level2.js
+++ src/pages/Level2.js
@@ -1,9 +1,49 @@
-import React from 'react'
+import React from "react";
+import "./styles/level2.css";
 
 const Level2 = () => {
-  return (
-    <div>Level2</div>
-  )
-}
+  const gameDescription =
+    "Unlock the secret! Decode the encrypted message bellow to progress.";
 
-export default Level2
+  const originalMessage = "It's not a bug, it's a feature.";
+
+  const encryptedMessage = originalMessage
+    .split("")
+    .map((char) =>
+      char === " "
+        ? " "
+        : char === "."
+        ? "."
+        : char === "'"
+        ? "'"
+        : String.fromCharCode(char.charCodeAt(0) + 3)
+    )
+    .join("");
+
+  console.log(encryptedMessage);
+
+  return (
+    <div className="gameDiv">
+      <div className="question">
+        <h3 className="header">{gameDescription}</h3>
+      </div>
+      <div className="message">
+        <h3 className="encryptedMessage">{encryptedMessage}</h3>
+      </div>
+      <div className="inputDiv">
+        <input
+          alt="Decipher the message here"
+          className="inputArea"
+          type="text"
+        />
+      </div>
+      <div className="checkButton">
+        <button type="submit" className="submitButton">
+          Decrypt & Submit
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default Level2;
